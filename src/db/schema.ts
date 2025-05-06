5f0d265b4782fd8a5bd9c7445175c75bff46aff4
domnantas <hello@domnantas.lt>
2025-05-06 13:56:55 +0300

    Revert "Update column names"
    This reverts commit 16c0f50d795855b1f11e05e85fe844df2b0e5dde.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,74 +3,74 @@ import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 // Better Auth schema
 
 export const user = pgTable("user", {
-	id: text().primaryKey(),
-	name: text().notNull(),
-	email: text().notNull().unique(),
-	email_verified: boolean().notNull(),
-	image: text(),
-	updated_at: timestamp().defaultNow(),
-	created_at: timestamp().defaultNow(),
+	id: text("id").primaryKey(),
+	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
+	emailVerified: boolean("email_verified").notNull(),
+	image: text("image"),
+	updatedAt: timestamp("updated_at").defaultNow(),
+	createdAt: timestamp("created_at").defaultNow(),
 });
 
 export const session = pgTable("session", {
-	id: text().primaryKey(),
-	expires_at: timestamp().notNull(),
-	token: text().notNull().unique(),
-	created_at: timestamp().notNull(),
-	updated_at: timestamp().notNull(),
-	ip_address: text(),
-	user_agent: text(),
-	user_id: text()
+	id: text("id").primaryKey(),
+	expiresAt: timestamp("expires_at").notNull(),
+	token: text("token").notNull().unique(),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
+	ipAddress: text("ip_address"),
+	userAgent: text("user_agent"),
+	userId: text("user_id")
 		.notNull()
 		.references(() => user.id),
 });
 
 export const account = pgTable("account", {
-	id: text().primaryKey(),
-	account_id: text().notNull(),
-	provider_id: text().notNull(),
-	user_id: text()
+	id: text("id").primaryKey(),
+	accountId: text("account_id").notNull(),
+	providerId: text("provider_id").notNull(),
+	userId: text("user_id")
 		.notNull()
 		.references(() => user.id),
-	access_token: text(),
-	refresh_token: text(),
-	id_token: text(),
-	access_token_expires_at: timestamp(),
-	refresh_token_expires_at: timestamp(),
-	scope: text(),
-	password: text(),
-	created_at: timestamp().notNull(),
-	updated_at: timestamp().notNull(),
+	accessToken: text("access_token"),
+	refreshToken: text("refresh_token"),
+	idToken: text("id_token"),
+	accessTokenExpiresAt: timestamp("access_token_expires_at"),
+	refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+	scope: text("scope"),
+	password: text("password"),
+	createdAt: timestamp("created_at").notNull(),
+	updatedAt: timestamp("updated_at").notNull(),
 });
 
 export const verification = pgTable("verification", {
-	id: text().primaryKey(),
-	identifier: text().notNull(),
-	value: text().notNull(),
-	expires_at: timestamp().notNull(),
-	created_at: timestamp(),
-	updated_at: timestamp(),
+	id: text("id").primaryKey(),
+	identifier: text("identifier").notNull(),
+	value: text("value").notNull(),
+	expiresAt: timestamp("expires_at").notNull(),
+	createdAt: timestamp("created_at"),
+	updatedAt: timestamp("updated_at"),
 });
 
 export const jwks = pgTable("jwks", {
-	id: text().primaryKey(),
-	public_key: text().notNull(),
-	private_key: text().notNull(),
-	created_at: timestamp().notNull(),
+	id: text("id").primaryKey(),
+	publicKey: text("public_key").notNull(),
+	privateKey: text("private_key").notNull(),
+	createdAt: timestamp("created_at").notNull(),
 });
 
 // WAL GO schema
 export const qso = pgTable("qso", {
-	id: text().primaryKey(),
-	activator_callsign: text().notNull(),
-	hunter_callsign: text().notNull(),
-	activator_square: text().notNull(),
-	hunter_square: text(),
-	band: text().notNull(),
-	mode: text().notNull(),
-	rst_sent: text(),
-	rst_received: text(),
-	qso_at: timestamp().defaultNow(),
-	updated_at: timestamp().defaultNow(),
-	created_at: timestamp().defaultNow(),
+	id: text("id").primaryKey(),
+	activatorCallsign: text("activator_callsign").notNull(),
+	hunterCallsign: text("hunter_callsign").notNull(),
+	activatorSquare: text("activator_square").notNull(),
+	hunterSquare: text("hunter_square"),
+	band: text("band").notNull(),
+	mode: text("mode").notNull(),
+	rstSent: text("rst_sent"),
+	rstReceived: text("rst_received"),
+	qsoAt: timestamp("qso_at").defaultNow(),
+	updatedAt: timestamp("updated_at").defaultNow(),
+	createdAt: timestamp("created_at").defaultNow(),
 });
