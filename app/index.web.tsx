3c3794d94190f064970e46c529d2b1afeaf3d005
domnantas <hello@domnantas.lt>
2025-05-17 01:23:01 +0300

    Remove One

--- app/index.web.tsx
+++
@@ -1,9 +0,0 @@
-import { H1, YStack } from "tamagui";
-
-export default function HomePage() {
-	return (
-		<YStack>
-			<H1 text="center">WAL GO</H1>
-		</YStack>
-	);
-}
--- app/map.tsx
+++
@@ -1,5 +0,0 @@
-import { Text } from "tamagui";
-
-export default function MapPage() {
-	return <Text>Map</Text>;
-}
