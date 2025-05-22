00592961d8b90b05cefef84ebd879c792cffd02f
domnantas <hello@domnantas.lt>
2025-05-22 19:42:57 +0300

    Set up builds

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,8 @@ import { createAuthClient } from "better-auth/react";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:8081",
+  // Disabling base url to make it work on physical devices
+  // baseURL: "",
   plugins: [
     expoClient({
       scheme: "walgo",
