a97006dea1efa7d38ad3ce1a04d8a6555d92ab4c
domnantas <hello@domnantas.lt>
2025-10-30 21:25:12 +0200

    Nuke everything

--- src/components/SplashScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { H1, YStack } from "tamagui";
-
-export const SplashScreen = () => {
-	return (
-		<YStack minH="100%" justify="center">
-			<H1 text="center">Loading</H1>
-		</YStack>
-	);
-};
