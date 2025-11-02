343c9073df27ba0b00be390cd6d133ed4ba54f78
domnantas <hello@domnantas.lt>
2025-11-02 18:34:00 +0200

    Set up navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { StatusBar } from "expo-status-bar";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <>
+      <StatusBar />
+      <NativeTabs>
+        <NativeTabs.Trigger name="index">
+          <Label>Map</Label>
+          <Icon sf="map" drawable="custom_android_drawable" />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="log">
+          <Label>Log</Label>
+          <Icon sf="list.bullet" drawable="custom_android_drawable" />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="settings">
+          <Icon sf="gear" drawable="custom_settings_drawable" />
+          <Label>Settings</Label>
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    </>
+  );
 }
