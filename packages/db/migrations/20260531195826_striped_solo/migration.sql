587f57d94ab247225fc3af657966d5b54e36309a
domnantas <hello@domnantas.lt>
2026-05-31 23:07:53 +0300

    Fix rate limit

--- packages/db/migrations/20260531195826_striped_solo/migration.sql
+++ packages/db/migrations/20260531195826_striped_solo/migration.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "rate_limit" ALTER COLUMN "id" SET DATA TYPE text USING "id"::text;--> statement-breakpoint
-ALTER TABLE "rate_limit" ALTER COLUMN "id" DROP IDENTITY;
+ALTER TABLE "rate_limit" ALTER COLUMN "id" DROP IDENTITY;--> statement-breakpoint
+ALTER TABLE "rate_limit" ALTER COLUMN "id" SET DATA TYPE text USING "id"::text;
