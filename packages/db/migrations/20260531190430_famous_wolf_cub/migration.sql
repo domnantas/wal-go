7750e7b0e2b0243c98476d30927bdbed95bd4855
domnantas <hello@domnantas.lt>
2026-05-31 22:09:58 +0300

    Truncate rate limit

--- packages/db/migrations/20260531190430_famous_wolf_cub/migration.sql
+++ packages/db/migrations/20260531190430_famous_wolf_cub/migration.sql
@@ -1,4 +1,5 @@
-ALTER TABLE "rate_limit" ADD COLUMN "id" text;--> statement-breakpoint
+TRUNCATE TABLE "rate_limit";--> statement-breakpoint
+ALTER TABLE "rate_limit" ADD COLUMN "id" text NOT NULL;--> statement-breakpoint
 ALTER TABLE "rate_limit" DROP CONSTRAINT "rate_limit_pkey";--> statement-breakpoint
 ALTER TABLE "rate_limit" ADD PRIMARY KEY ("id");--> statement-breakpoint
 ALTER TABLE "rate_limit" ADD CONSTRAINT "rate_limit_key_key" UNIQUE("key");
